30a76d49f5da18579f43d51d4db38652b7e82864
Syenzkyy <107863901+Syenzkyy@users.noreply.github.com>
2022-08-23 19:38:59 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,23 +25,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.botname = "𝘀𝘆𝗲𝗻𝘇𝗸𝘆 | 𝗧𝗶𝗸𝘁𝗼𝗸 𝗩𝟵" //namabot kalian
+global.ownername= "Syenzky" //nama kalian
+global.myweb ="https://instagram.com/syenzky" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/akiravfx._" //bebas asal jan hapus
+global.myweb2 = "https://linktr.ee/syenzky" //bebas
+global.email = "syenzkypubgmy@gmail.com" //bebas
+global.region = "Malaysia" //bebas
+global.timezone = 'Asia/Kuala Lumpur' //  timezone wib
+global.premium = ['60109561479'] //premium user
+global.owner = ["60109561479","601135216064"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "60109561479" // nomor wa kalian
+global.ownernomerr = "+60109561479" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '𝘀𝘆𝗲𝗻𝘇𝗸𝘆 𝘆𝗮𝗻𝗴 𝗯𝘂𝗮𝘁 𝗵𝗲𝗵𝗲' //sticker wm ubah
+global.author = '𝘀𝘆𝗲𝗻𝘇𝗸𝘆 !' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
